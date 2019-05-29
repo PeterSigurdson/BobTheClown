32901cb8d69e1738671fc3189e83e2424feca021
Peter Sigurdson <peter@petersigurdson.net>
2019-05-29 09:35:01 -0400

    ddd

--- C099999/C099999/Program.cs
+++ C099999/C099999/Program.cs
@@ -10,6 +10,27 @@ namespace C099999
     {
         static void Main(string[] args)
         {
+            Dog d1 = new Dog();
+            d1.name = "Fido";
+            d1.age = 3;
+            d1.SayHello();
+        }
+    }
+    class Dog
+    {
+        Random rand = new Random();
+        public string name;
+        public int age;
+        public int Strength;
+
+        public Dog()
+        {
+            Strength = rand.Next(1, 100);
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("My name is {0}  My strength is {1} ", name, Strength);
         }
     }
 }
